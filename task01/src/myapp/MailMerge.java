3fe00b5a75ae0629449c82d95612fd6bfb5b3ffe
Jason Lee Jian Ying <79128355+Jasonljy90@users.noreply.github.com>
2022-12-21 08:27:47 +0800

    Fixed tour_packages files.
    Fixed issue for not being able to read and print other csv and txt files correctly. Although, test over and i am disappointed with my result. I have managed to solved this and ready to move on to focus and work harder. I shall be more vigilant

--- task01/src/myapp/MailMerge.java
+++ task01/src/myapp/MailMerge.java
@@ -4,35 +4,36 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 
 public class MailMerge {
+   
     public void mailMerge(String csvFile, String templateFile) throws IOException {
 
-        // Read the csv file
+        // Read the csv file and get the ArrayList
         ArrayList<Map<String, String>> rows = readCsvFile(csvFile);
-
 
         for (Map<String, String> row : rows) {
             FileReader fr = new FileReader(templateFile);
             BufferedReader br = new BufferedReader(fr);
             String line;
+           
             while ((line = br.readLine()) != null) {
-                
+               
                 for (Entry<String, String> entry : row.entrySet()) {
                     String key = entry.getKey();
+                    //System.out.println("Key " + key);
                     String value = entry.getValue();
-            
+                    
                     if (line.contains(key)) {
                         String tag = String.format("<<%s>>", key);
-                        String newLine = line.replace(tag, value);
-                        newLine = newLine.replaceAll("\\\\n", "\n");
-                        System.out.println(newLine);
-                        System.out.println();
+                        line = line.replaceAll(tag, value);
+                        line = line.replaceAll("\\\\n", "\n");
                     }
                 }
+                System.out.println(line);
             }
             br.close();
         }
@@ -42,7 +43,6 @@ public class MailMerge {
 
         BufferedReader br = new BufferedReader(new FileReader(csvFile));
 
-        // Skip the first line of csv files which stores the Header names
         String firstLine = br.readLine();
         ArrayList<Map<String, String>> rows = new ArrayList<Map<String, String>>();
         String[] headers = firstLine.split(",");
@@ -50,7 +50,6 @@ public class MailMerge {
         String line;
 
         while ((line = br.readLine()) != null) {
-            // Spilt the line in order to get publisher name
             String[] csvRow = line.split(",");
             Map<String, String> row = new HashMap<String, String>();
             for (int i = 0; i < csvRow.length; i++) {
